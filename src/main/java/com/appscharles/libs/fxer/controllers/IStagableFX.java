5543b9d9d91185f0b691aca9fc1b5664619978c1
appscharles <appscharlescom@gmail.com>
2018-08-25 09:15:58 +0200

    IStagableFX

---
+++ src/main/java/com/appscharles/libs/fxer/controllers/IStagableFX.java
@@ -0,0 +1,13 @@
+package com.appscharles.libs.fxer.controllers;
+
+/**
+ * IDE Editor: IntelliJ IDEA
+ * <p>
+ * Date: 03.07.2018
+ * Time: 14:59
+ * Project name: fxer
+ *
+ * @author Karol Golec karol.itgolo@gmail.com
+ */
+public interface IStagableFX extends IGetStagableFX, ISetStagableFX {
+}
