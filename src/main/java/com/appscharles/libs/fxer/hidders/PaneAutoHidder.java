94b2bdc39c58919bbd3820f2a45954759262fb2b
appscharles <appscharlescom@gmail.com>
2018-09-19 14:44:56 +0200

    PaneAutoHidder

---
+++ src/main/java/com/appscharles/libs/fxer/hidders/PaneAutoHidder.java
@@ -0,0 +1,41 @@
+package com.appscharles.libs.fxer.hidders;
+
+import com.google.common.collect.Lists;
+import javafx.collections.ListChangeListener;
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+
+import java.util.List;
+
+/**
+ * The type Pane auto hidder.
+ */
+public class PaneAutoHidder {
+
+    /**
+     * Auto hide.
+     *
+     * @param pane the pane
+     */
+    public static void autoHide(Pane pane){
+        List<Node> assigned = Lists.newArrayList();
+        pane.getChildren().addListener((ListChangeListener<Node>) list->{
+            for (Node child : list.getList()) {
+                if (assigned.contains(child)){
+                    continue;
+                }
+                child.visibleProperty().addListener((args, oldVal, newVal)->{
+                    Boolean childVisible = false;
+                    for (Node widgetsContainerPaneChild : pane.getChildren()) {
+                        if (widgetsContainerPaneChild.isVisible()){
+                            childVisible = true;
+                        }
+                    }
+                    pane.setVisible(childVisible);
+                    pane.setManaged(childVisible);
+                });
+                assigned.add(child);
+            }
+        });
+    }
+}
