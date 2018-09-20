c599ede4d8a570e11fedabd6039d6bd557aede3c
appscharles <appscharlescom@gmail.com>
2018-09-20 08:36:26 +0200

    PaneAutoHidder

--- src/main/java/com/appscharles/libs/fxer/hidders/PaneAutoHidder.java
+++ src/main/java/com/appscharles/libs/fxer/hidders/PaneAutoHidder.java
@@ -20,22 +20,29 @@ public class PaneAutoHidder {
     public static void autoHide(Pane pane){
         List<Node> assigned = Lists.newArrayList();
         pane.getChildren().addListener((ListChangeListener<Node>) list->{
-            for (Node child : list.getList()) {
-                if (assigned.contains(child)){
-                    continue;
-                }
-                child.visibleProperty().addListener((args, oldVal, newVal)->{
-                    Boolean childVisible = false;
-                    for (Node widgetsContainerPaneChild : pane.getChildren()) {
-                        if (widgetsContainerPaneChild.isVisible()){
-                            childVisible = true;
-                        }
-                    }
-                    pane.setVisible(childVisible);
-                    pane.setManaged(childVisible);
-                });
-                assigned.add(child);
-            }
+            assign(pane, assigned,  Lists.newArrayList(list.getList()));
         });
+        assign(pane, assigned, pane.getChildren());
     }
+
+    private static void assign(Pane pane, List<Node> assigned, List<Node> list) {
+        for (Node child : list) {
+            if (assigned.contains(child)){
+                continue;
+            }
+            child.visibleProperty().addListener((args, oldVal, newVal)->{
+                Boolean childVisible = false;
+                for (Node widgetsContainerPaneChild : pane.getChildren()) {
+                    if (widgetsContainerPaneChild.isVisible()){
+                        childVisible = true;
+                    }
+                }
+                pane.setVisible(childVisible);
+                pane.setManaged(childVisible);
+            });
+            assigned.add(child);
+        }
+    }
+
+
 }
